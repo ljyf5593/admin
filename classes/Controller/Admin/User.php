92dee9d2c0da2a4f15ec32b1d326a539335359c0
Liu Jie <ljyf5593@gmail.com>
2014-05-06 14:28:38 +0800

    fixed user dateline

--- classes/Controller/Admin/User.php
+++ classes/Controller/Admin/User.php
@@ -23,7 +23,7 @@ class Controller_Admin_User extends Controller_Admin_Crud {
 		$id = intval($this->request->param('id'));
 		$model = ORM::factory('User', $id);
 		$post_data = $this->request->post();
-		$post_data['dateline'] = strtotime($post_data['dateline']);
+		$post_data['dateline'] = isset($post_data['dateline'])?strtotime($post_data['last_login']):time();
 		$post_data['last_login'] = strtotime($post_data['last_login']);
 
 		try{
